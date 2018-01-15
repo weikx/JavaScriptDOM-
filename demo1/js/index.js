ddc52a847eb73560ece1023a35b103201103874e
weikexin <2099566778@qq.com>
2018-01-15 10:15:31 +0800

    分离js和html

--- demo1/js/index.js
+++ demo1/js/index.js
@@ -1,3 +1,5 @@
+"use strict"
+window.onload = perpareGallery;
 function showPic(whichPic) {
     var src = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
@@ -5,4 +7,24 @@ function showPic(whichPic) {
     var text = whichPic.getAttribute("title");
     var description = document.getElementById("description");
     description.firstChild.nodeValue = text;
+}
+function perpareGallery() {
+    // 判断是否支持这两个方法.... 好多余啊..
+    if (!document.getElementById) {
+        return false;
+    }
+    if (!document.getElementsByTagName) {
+        return fasle;
+    }
+    if (!document.getElementById("imagegallery")) {
+        return fasle;
+    }
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            showPic(this);
+            return false;
+        }
+    }
 }
